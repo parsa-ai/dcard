83b9088404907d29fba104fe7cf88cee178bf085
parsa alavi <parsaalavi9@gmail.com>
2025-12-19 18:01:29 +0330

    fix: make ok loction icons

--- app/components/MapPicker.tsx
+++ app/components/MapPicker.tsx
@@ -8,18 +8,8 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-geosearch/dist/geosearch.css';
 
-// فیکس آیکون مارکر
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
-
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
 // کامپوننت کلیک روی نقشه
 function MapClickHandler({ onPositionChange }: { onPositionChange: (pos: [number, number]) => void }) {
   useMapEvents({
